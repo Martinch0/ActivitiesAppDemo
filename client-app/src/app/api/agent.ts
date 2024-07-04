2b1d6229a72c23b67683dfc0b7f2ba7e24c8bf09
Martin Mihov <Martinch0@users.noreply.github.com>
2024-07-04 22:02:57 +0300

    Add requests account

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { Activity } from "../models/activity";
 import { toast } from "react-toastify";
 import { router } from "../router/Routes";
 import { store } from "../stores/store";
+import { User, UserFormValues } from "../models/user";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -74,8 +75,15 @@ const Activities = {
     delete: (id: string) => requests.del<void>(`/activities/${id}`)
 }
 
+const Account = {
+    current: () => requests.get<User>('/account'),
+    login: (user: UserFormValues) => requests.post('account/login', user),
+    register: (user: UserFormValues) => requests.post('account/register', user)
+}
+
 const agent = {
-    Activities
+    Activities,
+    Account
 }
 
 export default agent;
