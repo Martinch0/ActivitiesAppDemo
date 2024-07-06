f593eab33b289f3575f0cf3b49200db33a5edd6f
Martin Mihov <Martinch0@users.noreply.github.com>
2024-07-06 13:06:31 +0300

    Hookup host and button display

--- client-app/src/app/models/activity.ts
+++ client-app/src/app/models/activity.ts
@@ -1,15 +1,18 @@
 import { Profile } from "./profile"
 
 export interface Activity {
-    id: string
-    title: string
-    date: Date | null
-    description: string
-    category: string
-    city: string
-    venue: string
-    hostUsername?: string
-    isCancelled?: boolean
-    attendees?: Profile[]
+    id: string;
+    title: string;
+    date: Date | null;
+    description: string;
+    category: string;
+    city: string;
+    venue: string;
+    hostUsername?: string;
+    isCancelled?: boolean;
+    isGoing?: boolean;
+    isHost?: boolean;
+    host?: Profile;
+    attendees?: Profile[];
   }
   
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -2,6 +2,7 @@ import { makeAutoObservable, runInAction } from "mobx";
 import { Activity } from "../models/activity";
 import agent from "../api/agent";
 import { format } from "date-fns";
+import { store } from "./store";
 
 export default class ActivityStore {
     activityRegistry = new Map<string, Activity>;
@@ -76,6 +77,12 @@ export default class ActivityStore {
     }
 
     private setActivity = (activity: Activity) => {
+        const user = store.userStore.user;
+        if(user) {
+            activity.isGoing = activity.attendees!.some(a => a.userName === user.userName);
+            activity.isHost = activity.hostUsername == user.userName;
+            activity.host = activity.attendees?.find(x => x.userName == activity.hostUsername);
+        }
         activity.date = new Date(activity.date!);
         this.activityRegistry.set(activity.id, activity);
     }
