70024d76d0f36e1a0d42343b573aab3034cb4050
Martin Mihov <Martinch0@users.noreply.github.com>
2024-07-06 13:44:43 +0300

    Fix Managing and Creating of activities.

--- client-app/src/app/models/activity.ts
+++ client-app/src/app/models/activity.ts
@@ -1,6 +1,6 @@
 import { Profile } from "./profile"
 
-export interface Activity {
+export interface IActivity {
     id: string;
     title: string;
     date: Date | null;
@@ -8,11 +8,61 @@ export interface Activity {
     category: string;
     city: string;
     venue: string;
-    hostUsername?: string;
-    isCancelled?: boolean;
-    isGoing?: boolean;
-    isHost?: boolean;
+    hostUsername: string;
+    isCancelled: boolean;
+    isGoing: boolean;
+    isHost: boolean;
     host?: Profile;
     attendees?: Profile[];
+  }
+
+  export class Activity implements IActivity {
+    constructor(activity: ActivityFormValues)
+    {
+      this.id = activity.id!;
+      this.title = activity.title;
+      this.category = activity.category;
+      this.description = activity.description;
+      this.date = activity.date;
+      this.city = activity.city;
+      this.venue = activity.venue;
+    }
+    
+    id: string;
+    title: string;
+    date: Date | null;
+    description: string;
+    category: string;
+    city: string;
+    venue: string;
+    hostUsername: string = '';
+    isCancelled: boolean = false;
+    isGoing: boolean = false;
+    isHost: boolean = false;
+    host?: Profile;
+    attendees?: Profile[];
+  }
+
+  export class ActivityFormValues {
+    id?: string = undefined;
+    title: string = '';
+    category: string = '';
+    description: string = '';
+    date: Date | null = null;
+    city: string = '';
+    venue: string = '';
+
+    constructor(activity?: ActivityFormValues)
+    {
+      if(activity) {
+        this.id = activity.id;
+        this.title = activity.title;
+        this.category = activity.category;
+        this.description = activity.description;
+        this.date = activity.date;
+        this.city = activity.city;
+        this.venue = activity.venue;
+      }
+    }
   }
   
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { Activity } from "../../../app/models/activity";
+import { ActivityFormValues } from "../../../app/models/activity";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { Formik, Form } from "formik";
 import * as Yup from 'yup';
@@ -16,19 +16,11 @@ import MyDateInput from "../../../app/common/form/MyDateInput";
 
 export default observer(function ActivityForm() {
     const {activityStore} = useStore();
-    const {loading, createActivity, updateActivity, loadActivity, loadingInitial} = activityStore;
+    const {createActivity, updateActivity, loadActivity, loadingInitial} = activityStore;
     const {id} = useParams();
     const navigate = useNavigate();
     
-    const [activity, setActivity] = useState<Activity>({
-        id: '',
-        title: '',
-        category: '',
-        description: '',
-        date: null,
-        city: '',
-        venue: ''
-    });
+    const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
     const validationSchema = Yup.object({
         title: Yup.string().required('Title is required'),
@@ -40,16 +32,19 @@ export default observer(function ActivityForm() {
     })
     
     useEffect(() => {
-        if(id) loadActivity(id).then(activity => setActivity(activity!))
+        if(id) loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity)))
     }, [id, loadActivity])
 
-    function handleFormSubmit(activity: Activity)
+    function handleFormSubmit(activity: ActivityFormValues)
     {
         if(activity.id) {
             updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
         } else {
-            activity.id = uuid();
-            createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+            const newActivity = {
+                ...activity,
+                id: uuid()
+            }
+            createActivity(newActivity).then(() => navigate(`/activities/${newActivity.id}`));
         }
     }
 
@@ -80,7 +75,7 @@ export default observer(function ActivityForm() {
                         <MyTextInput placeholder='Venue' name='venue'/>
                         <Button
                             disabled={isSubmitting || !isValid || !dirty}
-                            loading={loading}
+                            loading={isSubmitting}
                             floated="right" positive type='submit' content='Submit' />
                         <Button as={Link} to='/activities' floated="right" type='button' content='Cancel' />
                     </Form>
